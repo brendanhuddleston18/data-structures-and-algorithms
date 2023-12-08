48a77b353e4793e93b627091f735498c7b6bd370
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-12-07 18:52:16 -0600

    added all four solutions

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -56,6 +56,8 @@ console.log(a) prints [1, 2, 3, 1]
 
 const appendFirstToLast = (arr) => {
   // Solution code here...
+  arr.push(arr[0]);
+
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -75,6 +77,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
