1a88fc09edf77a03f0be3a75fed90c2a2891b334
Brendan Huddleston <brendanhuddleston18@gmail.com>
2024-02-21 18:36:03 -0600

    added solution, one test fail

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,14 @@
 from data_structures.hashtable import Hashtable
 
 
-def left_join():
-    pass
+def left_join(syns, ants):
+
+    word_list = []
+
+    for key in syns:
+        if key not in ants:
+            word_list.append([key, syns[key], None])
+        else:
+            word_list.append([key, syns[key], ants[key]])
+
+    return word_list
--- python/tests/code_challenges/test_hashtable_left_join.py
+++ python/tests/code_challenges/test_hashtable_left_join.py
@@ -6,7 +6,7 @@ def test_exists():
     assert left_join
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_example():
     synonyms = {
         "diligent": "employed",
@@ -24,13 +24,34 @@ def test_example():
     }
 
     expected = [
-        ["fond", "enamored", "averse"],
-        ["wrath", "anger", "delight"],
         ["diligent", "employed", "idle"],
-        ["outfit", "garb", "NONE"],
+        ["fond", "enamored", "averse"],
         ["guide", "usher", "follow"],
+        ["wrath", "anger", "delight"],
+        ["outfit", "garb", "NONE"],
     ]
 
     actual = left_join(synonyms, antonyms)
 
     assert actual == expected
+
+# @pytest.mark.skip("TODO")
+def test_left_join():
+    cinnamons = {"happy": "joyful", "sad": "sorrowful"}
+    auntieannes = {"happy": "sad", "angry": "calm"}
+    expected = [["happy", "joyful", "sad"], ["sad", "sorrowful", None]]
+    assert left_join(cinnamons, auntieannes) == expected
+
+# @pytest.mark.skip("TODO")
+def test_key_in_both():
+    cinnamons = {"fast": "quick", "slow": "sluggish"}
+    auntieannes = {"fast": "slow", "bright": "dim"}
+    expected = [["fast", "quick", "slow"], ["slow", "sluggish", None]]
+    assert left_join(cinnamons, auntieannes) == expected
+
+# @pytest.mark.skip("TODO")
+def test_key_in_first():
+    cinnamons = {"light": "bright", "dark": "dim"}
+    auntieannes = {"heavy": "light", "shallow": "deep"}
+    expected = [["light", "bright", None], ["dark", "dim", None]]
+    assert left_join(cinnamons, auntieannes) == expected
